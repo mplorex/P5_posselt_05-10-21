8a2d6a4f92689376a6138f55ce2fe47391e30eb8
michael posselt <michaelposselt@outlook.com>
2021-07-07 12:41:28 -0500

    site

--- javascript/products.js
+++ javascript/products.js
@@ -1,48 +1,48 @@
 fetch('http://localhost:3000/api/teddies')
 .then(response => {
-    return response.json()
+    return response.json();
 })
 .then(teddies => {
-    const teddyList = document.getElementById('teddies')
+    const teddyList = document.getElementById('teddies');
     for (let i = 0; i < teddies.length; i++) {
         console.log(teddies[i]);
-        const col = document.createElement('div')
-        const card = document.createElement('div')
-        const cardImg = document.createElement('img')
-        const cardBody = document.createElement('div')
-        const cardTitle = document.createElement('h5')
-        const cardText = document.createElement('p')
-        const cardPrice = document.createElement('div')
-        const cardBtn = document.createElement('a')
+        const col = document.createElement('div');
+        const card = document.createElement('div');
+        const cardImg = document.createElement('img');
+        const cardBody = document.createElement('div');
+        const cardTitle = document.createElement('h5');
+        const cardText = document.createElement('p');
+        const cardPrice = document.createElement('div');
+        const cardBtn = document.createElement('a');
 
         cardBtn.innerHTML = "Buy Now!";
 
-        col.setAttribute('class', 'col-12 col-sm-4 mb-4')
-        card.setAttribute('class', 'card')
+        col.setAttribute('class', 'col-12 col-sm-4 mb-4');
+        card.setAttribute('class', 'card');
         card.setAttribute('onClick', 'goToTeddy("'+teddies[i]._id+'")');
-        cardImg.setAttribute('class', 'card-img-top')
-        cardBody.setAttribute('class', 'card-body')
-        cardTitle.setAttribute('class', 'card-title')
-        cardText.setAttribute('class', 'card-text')
-        cardPrice.setAttribute('class', 'card-price')
-        cardBtn.setAttribute('class', 'btn btn-primary stretched-link')
+        cardImg.setAttribute('class', 'card-img-top');
+        cardBody.setAttribute('class', 'card-body');
+        cardTitle.setAttribute('class', 'card-title');
+        cardText.setAttribute('class', 'card-text');
+        cardPrice.setAttribute('class', 'card-price');
+        cardBtn.setAttribute('class', 'btn btn-primary stretched-link');
 
-        cardImg.src = teddies[i].imageUrl 
-        cardTitle.textContent = teddies[i].name
-        cardText.textContent = teddies [i].description
-        cardPrice.textContent = teddies [i].price
+        cardImg.src = teddies[i].imageUrl ;
+        cardTitle.textContent = teddies[i].name;
+        cardText.textContent = teddies [i].description;
+        cardPrice.textContent = teddies [i].price;
 
-        col.appendChild(card)
-        card.appendChild(cardImg)
-        card.appendChild(cardBody)
-        cardBody.appendChild(cardTitle)
-        cardBody.appendChild(cardText)
-        cardBody.appendChild(cardPrice)
-        cardBody.appendChild(cardBtn)
+        col.appendChild(card);
+        card.appendChild(cardImg);
+        card.appendChild(cardBody);
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        cardBody.appendChild(cardPrice);
+        cardBody.appendChild(cardBtn);
         teddyList.appendChild(col);
     }
 })
-.catch (error => console.error(error))
+.catch (error => console.error(error));
 
 function goToTeddy(id){
     window.location.href = "teddy.html?id=" + id;
