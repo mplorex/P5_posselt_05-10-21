dd267b12a109c34e8286a14641c71c5f0c1dd9e6
michael posselt <michaelposselt@outlook.com>
2021-08-09 00:01:57 -0500

    error deletion and checkout page creation

---
+++ javascript/checkout.js
@@ -0,0 +1,43 @@
+const checkoutCol = document.createElement('div');
+const checkoutHeader = document.createElement('h4');
+const checkoutForm = document.createElement('form');
+
+const formRow = document.createElement('div');
+
+const formColFirst = document.createElement('div');
+const formLabelFirst = document.createElement('label');
+const formInputFirst = document.createElement('input');
+const formFeedbackFirst = document.createElement('div')
+
+const formColLast = document.createElement('div');
+const formLabelLast = document.createElement('label');
+const formInputLast = document.createElement('input');
+const formFeedbackLast = document.createElement('div')
+
+const formEmail = document.createElement('div');
+const formLabelEmail = document.createElement('label');
+const formLabelSpan = document.createElement('span');
+const formInputEmail = document.createElement('input');
+const formFeedbackEmail = document.createElement('div');
+
+const formAddress = document.createElement('div');
+const formLabelAddress = document.createElement('label');
+const formInputAddress = document.createElement('input');
+const formFeedbackAddress = document.createElement('div');
+
+const fomrRowCity = document.createElement('div');
+const formColCity = document.createElement('div');
+const formLabelCity = document.createElement('label');
+const formInputCity = document.createElement('input');
+const formFeedbackCity = document.createElement('div');
+const formColZip = document.createElement('div');
+const formLabelZip = document.createElement('label');
+const formInputZip = document.createElement('input');
+const formFeedbackZip = document.createElement('div');
+
+const formLinebreak = document.createElement('hr');
+const formButton = document.createElement('button');
+
+
+
+
