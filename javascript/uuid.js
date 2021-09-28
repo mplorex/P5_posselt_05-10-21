56a5c0c86c7181654eb4ee74aa5771e6bc2dfcd7
michael posselt <michaelposselt@outlook.com>
2021-09-27 23:44:31 -0500

    uuid integration

--- javascript/uuid.js
+++ javascript/uuid.js
@@ -1,9 +1,28 @@
+const express = require('express')
 const uuid = require('uuid');
 
-router.post('/', (req, res) => {
+//body parse
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+//route get
+router.get('/:_id')
+app.get('/', function(req, res){
+    res.send('Hello world')
+})
+
+//route post
+router.post('/order', (req, res) => {
     const orderId = {
         ...req.body,
         id: uuid.v4(),
         status: 'active'
     };
 })
+
+//api route
+app.use('', require('./routes/'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('Server started on port ${PORT}'));
