b530f272aa1965b4e4a218a91fe6f8d34716bb7a
michael posselt <michaelposselt@outlook.com>
2021-10-05 10:14:53 -0500

    footer renaming refactoring

--- javascript/footer.js
+++ javascript/footer.js
@@ -1,51 +1,51 @@
 const footer = document.getElementById('footer')
 
-const ctnFooter = document.createElement('div')
-const ftrText = document.createElement('div')
-const ftrTitle = document.createElement('h5')
-const ftrBTitle = document.createElement('b')
-const ftrDescription = document.createElement('p')
-const ftrLinks = document.createElement('div')
-const ftrLink1 = document.createElement('a')
-const ftrLink11 = document.createElement('i')
-const ftrLink2 = document.createElement('a')
-const ftrLink22 = document.createElement('i')
-const ftrLink3 = document.createElement('a')
-const ftrLink33 = document.createElement('i')
+const footerContainer = document.createElement('div')
+const footerText = document.createElement('div')
+const footerTitle = document.createElement('h5')
+const footerTitle2 = document.createElement('b')
+const footerDescription = document.createElement('p')
+const footerLinks = document.createElement('div')
+const footerLink1A = document.createElement('a')
+const footerLink1AI = document.createElement('i')
+const footerLink2A = document.createElement('a')
+const footerLink2AI = document.createElement('i')
+const footerLink3A = document.createElement('a')
+const footerLink3AI = document.createElement('i')
 
-ftrBTitle.innerHTML = 'Orinoco'
-ftrDescription.innerHTML = 'Orinoco offers specific, themed apps which each sell one group of products.'
-ftrLinks.innerHTML = "Follow us on our Social Medias: "
-ftrLink1.innerHTML = ' Facebook '
-ftrLink2.innerHTML = ' Instagram '
-ftrLink3.innerHTML = ' Twitter '
+footerTitle2.innerHTML = 'Orinoco'
+footerDescription.innerHTML = 'Orinoco offers specific, themed apps which each sell one group of products.'
+footerLinks.innerHTML = "Follow us on our Social Medias: "
+footerLink1A.innerHTML = ' Facebook '
+footerLink2A.innerHTML = ' Instagram '
+footerLink3A.innerHTML = ' Twitter '
 
-ctnFooter.setAttribute('class', 'col bckg text-white container-xxl p-4')
-ftrLink1.setAttribute('href', 'https://facebook.com')
-ftrLink1.setAttribute('class', 'text-white')
-ftrLink1.setAttribute('style', 'text-decoration:none')
-ftrLink11.setAttribute('class', 'fab fa-facebook')
-ftrLink2.setAttribute('href', 'https://instagram.com')
-ftrLink2.setAttribute('class', 'text-white')
-ftrLink2.setAttribute('style', 'text-decoration:none')
-ftrLink22.setAttribute('class', 'fab fa-instagram')
-ftrLink3.setAttribute('href', 'https://twitter.com')
-ftrLink3.setAttribute('class', 'text-white')
-ftrLink3.setAttribute('style', 'text-decoration:none')
-ftrLink33.setAttribute('class', 'fab fa-twitter')
+footerContainer.setAttribute('class', 'col bckg text-white container-xxl p-4')
+footerLink1A.setAttribute('href', 'https://facebook.com')
+footerLink1A.setAttribute('class', 'text-white')
+footerLink1A.setAttribute('style', 'text-decoration:none')
+footerLink1AI.setAttribute('class', 'fab fa-facebook')
+footerLink2A.setAttribute('href', 'https://instagram.com')
+footerLink2A.setAttribute('class', 'text-white')
+footerLink2A.setAttribute('style', 'text-decoration:none')
+footerLink2AI.setAttribute('class', 'fab fa-instagram')
+footerLink3A.setAttribute('href', 'https://twitter.com')
+footerLink3A.setAttribute('class', 'text-white')
+footerLink3A.setAttribute('style', 'text-decoration:none')
+footerLink3AI.setAttribute('class', 'fab fa-twitter')
 
-footer.appendChild(ctnFooter)
-ctnFooter.appendChild(ftrText)
-ftrText.appendChild(ftrTitle)
-ftrTitle.appendChild(ftrBTitle)
-ftrText.appendChild(ftrDescription)
-ctnFooter.appendChild(ftrLinks)
-ftrLinks.appendChild(ftrLink1)
-ftrLink1.appendChild(ftrLink11)
-ftrLinks.appendChild(ftrLink2)
-ftrLink2.appendChild(ftrLink22)
-ftrLinks.appendChild(ftrLink3)
-ftrLink3.appendChild(ftrLink33);
+footer.appendChild(footerContainer)
+footerContainer.appendChild(footerText)
+footerText.appendChild(footerTitle)
+footerTitle.appendChild(footerTitle2)
+footerText.appendChild(footerDescription)
+footerContainer.appendChild(footerLinks)
+footerLinks.appendChild(footerLink1A)
+footerLink1A.appendChild(footerLink1AI)
+footerLinks.appendChild(footerLink2A)
+footerLink2A.appendChild(footerLink2AI)
+footerLinks.appendChild(footerLink3A)
+footerLink3A.appendChild(footerLink3AI);
 
 console.log(footer)
 
