bdefe67e77dd4b9bb8182089c31c50fe367133ff
michael posselt <michaelposselt@outlook.com>
2021-10-07 11:44:08 -0500

    checkout-btns.js name refactoring

--- javascript/cart/checkout-btns.js
+++ javascript/cart/checkout-btns.js
@@ -1,33 +1,33 @@
-const checkoutBtns = document.getElementById('checkout-btns');
+const cartButtons = document.getElementById('checkout-btns');
 
-const checkoutBtn = document.createElement('button');
-const checkoutBtnA = document.createElement('a')
-const emptyCartBtn = document.createElement('button');
-const cartRowTotal = document.createElement('div');
+const checkoutButton = document.createElement('button');
+const checkoutButtonA = document.createElement('a')
+const emptyCartButton = document.createElement('button');
+const cartPageTotal = document.createElement('div');
 
 
-checkoutBtn.setAttribute('class', 'btn btn-primary m-2 text-white');
-checkoutBtn.setAttribute('id', 'checkout')
-checkoutBtnA.setAttribute('href', 'checkout-form.html')
-emptyCartBtn.setAttribute('class', 'btn btn-primary m-2 text-white');
-cartRowTotal.setAttribute('id', 'cartTotal');
+checkoutButton.setAttribute('class', 'btn btn-primary m-2 text-white');
+checkoutButton.setAttribute('id', 'checkout')
+checkoutButtonA.setAttribute('href', 'checkout-form.html')
+emptyCartButton.setAttribute('class', 'btn btn-primary m-2 text-white');
+cartPageTotal.setAttribute('id', 'cartTotal');
 
 const priceTotal = document.getElementById('cartTotal');
 
-emptyCartBtn.addEventListener('click', emptyCart);
+emptyCartButton.addEventListener('click', emptyCart);
 
-checkoutBtn.innerHTML = 'Complete order'; 
-emptyCartBtn.innerHTML = 'Empty cart';
-cartRowTotal.innerHTML = '<b>Total: </b>' + new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD' }).format(cartTotal());
+checkoutButton.innerHTML = 'Complete order'; 
+emptyCartButton.innerHTML = 'Empty cart';
+cartPageTotal.innerHTML = '<b>Total: </b>' + new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD' }).format(cartTotal());
 
-checkoutBtn.addEventListener('click', ()=>{
+checkoutButton.addEventListener('click', ()=>{
     checkoutLink();
 })
 
-checkoutBtns.appendChild(checkoutBtnA);
-checkoutBtnA.appendChild(checkoutBtn);
-checkoutBtns.appendChild(emptyCartBtn);
-checkoutBtns.appendChild(cartRowTotal);
+cartButtons.appendChild(checkoutButtonA);
+checkoutButtonA.appendChild(checkoutButton);
+cartButtons.appendChild(emptyCartButton);
+cartButtons.appendChild(cartRowTotal);
 
 function emptyCart() {
     if(sessionStorage.getItem('cartData'));
@@ -46,4 +46,4 @@ function checkoutLink() {
     btnClick.href = 'cart.html';
 }
 
-console.log(checkoutBtns);
+console.log(cartButtons);
